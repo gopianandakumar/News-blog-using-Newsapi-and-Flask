0c07405261ab09fe340b45d559b6d1516427503b
gopianandanaidu <66548718+gopianandakumar@users.noreply.github.com>
2021-06-24 15:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 from newsapi import NewsApiClient
 
 # Init
-newsapi = NewsApiClient(api_key='c005c347b5fc41b79760188b38aa5812')
+newsapi = NewsApiClient(api_key='you can use your news api key')
 
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(q='india',
@@ -13,10 +13,7 @@ top_headlines = newsapi.get_top_headlines(q='india',
 
 
 data = top_headlines['articles']
-#news =[]
 
-# for i in data:
-# 	news.append(i['title'])
 
 
 
